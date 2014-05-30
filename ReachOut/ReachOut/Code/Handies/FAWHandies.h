342a738ba47b02b40fbad694c09e4b187fa4c835
cze <piotr.nietrzebka@gmail.com>
2014-05-30 02:59:29 +0200

    Initial project structure and main iPhone storyboard.

---
+++ ReachOut/ReachOut/Code/Handies/FAWHandies.h
@@ -0,0 +1,13 @@
+//
+//  FAWHandies.h
+//  ReachOut
+//
+//  Created by czesio on 30.05.2014.
+//  Copyright (c) 2014 Far and Wide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FAWHandies : NSObject
+
+@end
--- ReachOut/ReachOut/FAWAppDelegate.m
+++ ReachOut/ReachOut/FAWAppDelegate.m
@@ -10,13 +10,9 @@
 
 @implementation FAWAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
-    return YES;
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+	
+	return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
